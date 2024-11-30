4976059c9d6f2db6c5d436e86faf8856c9fc50f9
dongzoolee <me@leed.at>
2024-11-30 10:25:58 +0900

    Set global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "./styles/fonts.scss";
+import "./styles/global.scss";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
